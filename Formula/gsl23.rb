ea8275e72cf59091981212cbb8ddb3ad706da9a4
acdc <acdc@acdcs-Mac.local>
2017-07-17 14:16:53 +0800

    A bottle for the older gsl 2.3 as the new gsl 2.4 does not provide libgsl.19.dylib which we need for our software.

---
+++ Formula/gsl23.rb
@@ -0,0 +1,20 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Gsl23 < Formula
+  desc "Numerical library for C and C++"
+  homepage "https://www.gnu.org/software/gsl/"
+  url "https://ftp.gnu.org/gnu/gsl/gsl-2.3.tar.gz"
+  sha256 "562500b789cd599b3a4f88547a7a3280538ab2ff4939504c8b4ac4ca25feadfb"
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make" # A GNU tool which doesn't support just make install! Shameful!
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gsl-randist", "0", "20", "cauchy", "30"
+  end
+end
